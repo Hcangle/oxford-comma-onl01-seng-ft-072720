f16256c5ad07d3e3b36fb9da92c087c96c31f339
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 05:01:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  case array.length 
-when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
-end 
+  return array.first if array.size == 1
+  if array.size == 2
+    array.insert(1, "and").join(" ")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
+end 	end 
